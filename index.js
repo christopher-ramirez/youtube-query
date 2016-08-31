f9627082de46c2748b95278329884ed6eda9db7c
Christopher <chris.ramirezg@gmail.com>
2016-08-30 21:37:09 -0600

    Implement class Video.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 var lib = require('./lib'),
     Youtube = lib.Youtube,
+    Video = lib.Video,
     Videos = lib.Videos;
 
 exports.Youtube = Youtube
+exports.Video = Video
 exports.Videos = Videos
